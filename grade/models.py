120ea8ab30565e8fcf8cb1f9f62dc7493c1327ec
richasdy <donni.richasdy@gmail.com>
2019-10-07 07:24:51 +0700

    add grade model

--- grade/models.py
+++ grade/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from audio.models import File
 
 # Create your models here.
+class Grade(models.Model):
+    File = models.ForeignKey(File, on_delete=models.SET_NULL, null=True)
+    # applicant_id = models.IntegerField(default=0) # in byte
+    action_grade = models.TextField(default="")
+    enthusiasm_grade = models.TextField(default="")
+    focus_grade = models.TextField(default="")
+    imagine_grade = models.TextField(default="")
+    integrity_grade = models.TextField(default="")
+    smart_grade = models.TextField(default="")
+    solid_grade = models.TextField(default="")
+    speed_grade = models.TextField(default="")
+    totality_grade = models.TextField(default="")
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
