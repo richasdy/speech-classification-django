97d9de9f9fd544d1f7f3c577be7340584a19c4f4
richasdy <donni.richasdy@gmail.com>
2019-10-07 07:23:54 +0700

    add transcribe model

--- speechclassification/settings.py
+++ speechclassification/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'audio.apps.AudioConfig',
+    'transcribe.apps.TranscribeConfig',
+    'grade.apps.GradeConfig',
 ]
 
 MIDDLEWARE = [
--- transcribe/models.py
+++ transcribe/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from audio.models import File
 
 # Create your models here.
+
+class Transcribe(models.Model):
+    File = models.ForeignKey(File, on_delete=models.SET_NULL, null=True)
+    # applicant_id = models.IntegerField(default=0) # in byte
+    action_text = models.TextField(default="")
+    enthusiasm_text = models.TextField(default="")
+    focus_text = models.TextField(default="")
+    imagine_text = models.TextField(default="")
+    integrity_text = models.TextField(default="")
+    smart_text = models.TextField(default="")
+    solid_text = models.TextField(default="")
+    speed_text = models.TextField(default="")
+    totality_text = models.TextField(default="")
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
