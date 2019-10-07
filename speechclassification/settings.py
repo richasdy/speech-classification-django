9636345c3c693322f187ba68f4f3bf4484a1095b
richasdy <donni.richasdy@gmail.com>
2019-10-08 06:33:53 +0700

    add heroku setting

--- speechclassification/settings.py
+++ speechclassification/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
@@ -129,3 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
