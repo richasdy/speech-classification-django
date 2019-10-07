120fba1becf4a759d31bd216489da191b856c7e5
richasdy <donni.richasdy@gmail.com>
2019-10-07 19:35:39 +0700

    add allow url

--- speechclassification/settings.py
+++ speechclassification/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '9*uf^gc6tx_)p5i_2g7^ka9%o_32%mg+tfd&^zn9+cs4wlwili'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'speech-classification-donni.herokuapp.com']
 
 
 # Application definition
