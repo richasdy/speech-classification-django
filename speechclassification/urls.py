56c99b2104e252bab655f3498cceabe529649f65
Donni Richasdy <richasdy@richasdys-MacBook-Pro.local>
2019-12-06 16:51:45 +0700

    remove debug setting

--- speechclassification/urls.py
+++ speechclassification/urls.py
@@ -26,9 +26,8 @@ urlpatterns = [
     path('grade/', include('grade.urls')),
     path('audio/', include('audio.urls')),
     path('transcribe/', include('transcribe.urls')),
-    path('admin/', admin.site.urls),
-    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),   
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
