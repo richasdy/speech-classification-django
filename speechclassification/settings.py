1feb462a2e212ae6a53f123d418bad5d6d513dc6
Donni Richasdy <richasdy@richasdys-MacBook-Pro.local>
2019-12-06 13:33:08 +0700

    remove heroku

--- speechclassification/settings.py
+++ speechclassification/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-import django_heroku
+# import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -135,4 +135,4 @@ STATICFILES_DIRS = [
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
